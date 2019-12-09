8010149c37ae2dbfd373bb626bc17b135c14eb36
noe nevarez <closner_nevarez@hotmail.com>
2019-12-09 09:50:15 +0700

    updated package.json

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "dcb47482ac58181a72e2";
+/******/ 	var hotCurrentHash = "3388a2d7d6658da4c129";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
